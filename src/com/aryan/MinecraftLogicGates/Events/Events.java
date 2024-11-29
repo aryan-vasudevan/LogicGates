26bfac876be9d07f1769be775b10da2bd9a56277
Aryan <vasudevan.aryan@gmail.com>
2024-11-29 17:30:09 -0500

    Mod working.

--- src/com/aryan/MinecraftLogicGates/Events/Events.java
+++ src/com/aryan/MinecraftLogicGates/Events/Events.java
@@ -1,15 +1,66 @@
-package com.aryan.MinecraftLogicGates.Events;
+package com.aryan.MinecraftLogicGates.Commands;
 
+import com.sk89q.worldedit.bukkit.WorldEditPlugin;
+import com.sk89q.worldedit.world.World;
+import com.sk89q.worldedit.bukkit.selections.Selection;
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldedit.schematic.SchematicFormat;
+import com.sk89q.worldedit.world.block.BlockState;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Bukkit;
 
-public class Events implements Listener{
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
 
-    @EventHandler
-    public static void onPlayerJoin(PlayerJoinEvent event) {
-        Player player = event.getPlayer();
-        player.
+public class SpawnCommand implements CommandExecutor {
+
+    private final WorldEditPlugin worldEdit;
+
+    public SpawnCommand(WorldEditPlugin worldEdit) {
+        this.worldEdit = worldEdit;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            File schematicFile = new File(worldEdit.getPlugin().getDataFolder() + "/schematics/spawn.schematic");
+
+            if (!schematicFile.exists()) {
+                player.sendMessage(ChatColor.RED + "Schematic not found!");
+                return false;
+            }
+
+            try {
+                // Load the schematic
+                com.sk89q.worldedit.schematic.Schematic schematic = SchematicFormat.MCEDIT.load(schematicFile);
+                World world = BukkitAdapter.adapt(player.getWorld());
+
+                // Get the player's position
+                BlockVector3 playerPos = BlockVector3.at(player.getLocation().getX(), player.getLocation().getY(), player.getLocation().getZ());
+                Selection selection = worldEdit.getSelection(player);
+
+                // Paste the schematic in front of the player
+                schematic.paste(world, playerPos, true);
+
+                player.sendMessage(ChatColor.GREEN + "Schematic pasted in front of you!");
+                return true;
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                player.sendMessage(ChatColor.RED + "Error loading the schematic.");
+                return false;
+            }
+        } else {
+            sender.sendMessage(ChatColor.RED + "Only players can execute this command.");
+            return false;
+        }
     }
 }
--- src/com/aryan/MinecraftLogicGates/MinecraftLogicGates.java
+++ src/com/aryan/MinecraftLogicGates/MinecraftLogicGates.java
@@ -2,11 +2,13 @@ package com.aryan.MinecraftLogicGates;
 
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+import com.aryan.MinecraftLogicGates.Events.Events;
 
 public class MinecraftLogicGates extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        getServer().getPluginManager().registerEvents(new Events(), this);
         getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[minecraftLogicGates]: Plugin is enabled!");
     }
 
